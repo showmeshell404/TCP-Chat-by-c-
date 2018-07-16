0733b9c5658c978585c1e64b582afaf853ef11d3
ShowMeShell <39519309+showmeshell404@users.noreply.github.com>
2018-07-16 17:02:46 +0800

    Add files via upload

---
+++ TcpClient/main.cpp
@@ -0,0 +1,13 @@
+﻿#include "tcpclient.h"
+#include <QApplication>
+#include <QTextCodec>
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QTextCodec *codec = QTextCodec::codecForName("utf-8");
+    QTextCodec::setCodecForTr(codec);
+    TcpClient w;
+    w.show();
+
+    return a.exec();
+}
